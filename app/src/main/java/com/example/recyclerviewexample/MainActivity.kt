888fc3843a178f04e6183dc7cbce0f4584032cb0
Jose Alonso <josear.dev@gmail.com>
2022-04-18 00:26:56 +0200

    Recycler View con ViewBinding

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -5,17 +5,19 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewexample.adapter.SuperHeroAdapter
+import com.example.recyclerviewexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initRecyclerView()
     }
 
     private fun initRecyclerView() {
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerSuperHero)
-        recyclerview.layoutManager = LinearLayoutManager(this)
-        recyclerview.adapter = SuperHeroAdapter(SuperHeroProvider.superheroList)
+        binding.recyclerSuperHero.layoutManager = LinearLayoutManager(this)
+        binding.recyclerSuperHero.adapter = SuperHeroAdapter(SuperHeroProvider.superheroList)
     }
 }
--- app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
@@ -7,19 +7,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recyclerviewexample.R
 import com.example.recyclerviewexample.SuperHero
+import com.example.recyclerviewexample.databinding.ItemSuperheroBinding
 
 class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
-    val superHero = view.findViewById<TextView>(R.id.tvSuperHeroName)
-    val realName = view.findViewById<TextView>(R.id.tvRealName)
-    val publisher = view.findViewById<TextView>(R.id.tvPublisher)
-    val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
+    val binding = ItemSuperheroBinding.bind(view)
 
     fun render(superHeroModel: SuperHero){
-        superHero.text = superHeroModel.superhero
-        realName.text  = superHeroModel.realName
-        publisher.text = superHeroModel.publisher
-        Glide.with(photo.context).load(superHeroModel.photo).into(photo)
+        binding.tvSuperHeroName.text = superHeroModel.superhero
+        binding.tvRealName.text  = superHeroModel.realName
+        binding.tvPublisher.text = superHeroModel.publisher
+        Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
     }
 
 }
