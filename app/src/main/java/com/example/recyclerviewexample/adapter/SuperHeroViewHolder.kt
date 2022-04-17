3a9a734c81225b0218138daca4282a74b1331e2f
Jose Alonso <josear.dev@gmail.com>
2022-04-18 00:12:28 +0200

    Se añade GLIDE para mostrar imagenes

--- app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
@@ -1,8 +1,10 @@
 package com.example.recyclerviewexample.adapter
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.recyclerviewexample.R
 import com.example.recyclerviewexample.SuperHero
 
@@ -11,14 +13,13 @@ class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
     val superHero = view.findViewById<TextView>(R.id.tvSuperHeroName)
     val realName = view.findViewById<TextView>(R.id.tvRealName)
     val publisher = view.findViewById<TextView>(R.id.tvPublisher)
-    //val photo = view.findViewById<TextView>(R.id.ivSuperHero)
+    val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
 
     fun render(superHeroModel: SuperHero){
         superHero.text = superHeroModel.superhero
         realName.text  = superHeroModel.realName
         publisher.text = superHeroModel.publisher
-
-
+        Glide.with(photo.context).load(superHeroModel.photo).into(photo)
     }
 
 }
