755ec4b875b1fe36d4942d1eb64f18a40f41eb8b
Jose Alonso <josear.dev@gmail.com>
2022-04-18 00:37:28 +0200

    Recycler SetOnclickListener

--- app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewexample/adapter/SuperHeroViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.recyclerviewexample.adapter
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recyclerviewexample.R
@@ -18,6 +19,12 @@ class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
         binding.tvRealName.text  = superHeroModel.realName
         binding.tvPublisher.text = superHeroModel.publisher
         Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
+
+        binding.ivSuperHero.setOnClickListener {
+            Toast.makeText(binding.ivSuperHero.context, superHeroModel.realName,Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
 }
